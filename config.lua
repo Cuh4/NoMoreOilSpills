52b4f4da13a9dd9a0eddd73c146558b7403b1f7f
Cuh4 <roozzguy@gmail.com>
2023-06-24 15:50:17 +0100

    no need for most stuffs

--- config.lua
+++ config.lua
@@ -1,27 +1,15 @@
 config = {
     info = {
-        addonName = "All Creatures Hate You",
-        author = "cuh4#7366",
+        addonName = "No More Oil Spills",
+        author = "cuh5_",
         invite = "https://discord.gg/zTQxaZjwDr",
-        helpMessage = "Watch out! All creatures will run after you if you get close! The Industrial Frontier DLC is also required.",
+        helpMessage = "Tired of the ocean being polluted with oil? No more! This addon cleans up all oil spills automatically.",
 
-        aridRequired = true,
+        aridRequired = false,
         weaponsRequired = false
     },
 
-    main = {
-        attackRange = property.slider("Creature Attack Range", 0.1, 4.0, 0.05, 2),
-        attackDamage = property.slider("Creature Attack Damage", 0.1, 100, 0.1, 12),
-        detectionRange = property.slider("Creature Detection Range", 1, 500, 0.1, 20),
-
-        unloadRange = 1000
-    },
-
-    misc = {
-        attackUI_duration = 0.4
-    },
-
-    isDedicatedServer = false,
+    isDedicatedServer = false, -- doesn't matter for this addon
     debugEnabled = true,
-    debugShouldLog = true,
+    debugShouldLog = true
 }
