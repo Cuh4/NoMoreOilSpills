947a8a4679f2e9f76d2f34a36626a1516ddc4120
Cuh4 <roozzguy@gmail.com>
2023-06-24 15:51:24 +0100

    toggle cleanup

--- p3_commands/toggle.lua
+++ p3_commands/toggle.lua
@@ -1,8 +1,7 @@
 ---------------------------------------
-------------- Command - Toggles whether or not creatures are hostile
+------------- [Command] Toggles whether or not oil spills are cleaned up
 ---------------------------------------
 
-------------- ?toggle
 ---@param command af_services_commands_command
 ---@param args table<integer, string>
 ---@param player af_services_player_player
@@ -11,18 +10,8 @@ AuroraFramework.services.commandService.create(function(command, args, player)
         return announceLibrary.status.failure("This command requires admin.", player)
     end
 
-    local enabled = creatureLibrary.control.isEnabled()
-    creatureLibrary.control.setEnabled(not enabled)
+    local enabled = oilSpillCleanupEnabled
+    oilSpillCleanupEnabled = not enabled
 
-    announceLibrary.status.success(
-        AuroraFramework.libraries.miscellaneous.switchbox(
-            "[Disabled] Creatures will now be peaceful.",
-            "[Enabled] Creatures will now be hostile.",
-            not enabled
-        ),
-
-        player
-    )
-
-    chatAnnounce(player.properties.name.." has "..AuroraFramework.libraries.miscellaneous.switchbox("disabled", "enabled", not enabled).." creature hostility.")
-end, "toggle", {"t"}, false, "Toggles global creature hostility.")
+    chatAnnounce(player.properties.name.." has "..AuroraFramework.libraries.miscellaneous.switchbox("disabled", "enabled", not enabled).." oil spill cleanup.")
+end, "oc_toggle", {"oct"}, false, "Toggles oil spill cleanup.")
